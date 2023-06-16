3c7e04ba57bb9b78b0cc418c6b68bbb8ef0964ae
NasTouhokuAkasaka <133617838+NasTouhokuAkasaka@users.noreply.github.com>
2023-06-16 10:48:12 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,19 @@
+let userHeight;
+userHeight = '身長';
+
+let userm;
+userm = 1.7;
+
+console.log(userHeight);
+console.log(userm);
+
+let userWeight;
+userWeight = '体重';
+
+let userKg;
+userKg = 68;
+
+console.log(userWeight);
+console.log(userKg);
+
+console.log(userKg / (userm * userm));
